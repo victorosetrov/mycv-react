7e0aeaab12553283171bed28cd2e5826aba3b91b
Viktor Osetrov <victorosetrov@gmail.com>
2024-10-13 18:06:55 +0100

    Add missing donutChartSection.css2

--- src/components/DonutChartSection.jsx
+++ src/components/DonutChartSection.jsx
@@ -6,7 +6,7 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-import './../styles/donutChartSection.css';
+import '../styles/donutChartSection.css';
 
 
 ChartJS.register(ArcElement, Tooltip, Legend);
