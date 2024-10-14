e125e3341913658ff6b689467bd09f895ce3df53
Viktor Osetrov <victorosetrov@gmail.com>
2024-10-14 21:47:49 +0100

    Optimize GA42

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import Header from './components/Header';
 import MainIntro from './components/MainIntro';
 import Footer from './components/Footer';
 
-function App() {
-  const [gtmLoaded, setGtmLoaded] = useState(false);
-
+function App () {
   useEffect(() => {
-    // Load GTM after the user scrolls or interacts with the page
-    const loadGTM = () => {
-      if (!gtmLoaded) {
-        const script = document.createElement('script');
-        script.src = "https://www.googletagmanager.com/gtag/js?id=G-TCZ8FM15EP";
-        script.async = true;
-        document.head.appendChild(script);
-
-        const inlineScript = document.createElement('script');
-        inlineScript.innerHTML = `
-          window.dataLayer = window.dataLayer || [];
-          function gtag() { dataLayer.push(arguments); }
-          gtag('js', new Date());
-          gtag('config', 'G-TCZ8FM15EP');
-        `;
-        document.head.appendChild(inlineScript);
-
-        setGtmLoaded(true);
-      }
-    };
-
-    // Event listener for user interaction
-    window.addEventListener('scroll', loadGTM);
-    window.addEventListener('click', loadGTM);
-
-    return () => {
-      window.removeEventListener('scroll', loadGTM);
-      window.removeEventListener('click', loadGTM);
-    };
-  }, [gtmLoaded]);
-
+    ReactGA.initialize('G-TCZ8FM15EP');
+    ReactGA.send('pageview');
+  }, []);
   return (
     <>
       <Header />
@@ -47,6 +18,6 @@ function App() {
       <Footer />
     </>
   );
-}
+};
 
 export default App;
