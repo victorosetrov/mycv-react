09b098d54bf14a7aaae0aee94122a07375468aa0
Viktor Osetrov <victorosetrov@gmail.com>
2024-10-14 21:20:51 +0100

    Added lazy-loading3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { useEffect, Suspense } from 'react';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga4';
+import Header from './components/Header';
+import MainIntro from './components/MainIntro';
+import Footer from './components/Footer';
 
-// Lazy load components
-const Header = React.lazy(() => import('./components/Header'));
-const MainIntro = React.lazy(() => import('./components/MainIntro'));
-const Footer = React.lazy(() => import('./components/Footer'));
-
-function App() {
+function App () {
   useEffect(() => {
     ReactGA.initialize('G-TCZ8FM15EP');
     ReactGA.send('pageview');
   }, []);
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <>
       <Header />
       <main>
         <MainIntro />
       </main>
       <Footer />
-    </Suspense>
+    </>
   );
-}
+};
 
 export default App;
