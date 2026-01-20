25d2d809519e8c6cb15c68aa332470b8f6db25c6
Viktor Osetrov <victorosetrov@gmail.com>
2026-01-20 16:46:08 +0000

    Fix job title in chart data

--- src/components/MainIntro.jsx
+++ src/components/MainIntro.jsx
@@ -4,7 +4,7 @@ const DonutChartSection = lazy(() => import('./DonutChartSection'));
 const EmailSubscription = lazy(() => import('./EmailSubscription'));
 
 const chartData = [
-  { label: "dbt labs - Customer Solution Engineer", value: 1, color: "#2f4f4f" },
+  { label: "dbt labs - Customer Solutions Engineer", value: 1, color: "#2f4f4f" },
   { label: "Intellync (AB Agri) - Senior Support Engineer", value: 19, color: "#9370db" },
   { label: "Zendesk - Technical Support Engineer", value: 31, color: "#f76c6c" },
   { label: "Google - Technical Support Analyst", value: 20, color: "#1bc98e" },
