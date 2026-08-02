ea7c85e90f3adae043a8640cfbe6a5b98298129e
Viktor Osetrov <victorosetrov@gmail.com>
2026-08-02 19:25:22 +0100

    Add GitHub link to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,6 +21,9 @@ const Footer = () => {
           <a href="https://www.instagram.com/viktorosetrov/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="text-[#B3C7FF]">
             <FontAwesomeIcon icon={faInstagram} className="w-6 h-6 hover:scale-110 transform transition-transform" />
           </a>
+          <a href="https://github.com/victorosetrov/mycv-react" target="_blank" rel="noopener noreferrer" aria-label="GitHub" className="text-[#B3C7FF]">
+            <FontAwesomeIcon icon={faGithub} className="w-6 h-6 hover:scale-110 transform transition-transform" />
+          </a>
         </div>
 
         {/* Footer Text */}
